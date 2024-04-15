e02d8c4f751df4df2fce3604dd6d030831236348
Nicolas Melo <jaimenicolasmelo@gmail.com>
2024-04-15 14:21:24 -0500

    Responsive styles, positioning

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,8 +13,8 @@ export const Home = ({cityInfo, weather}) => {
 
   return (
     <>
-      <section className='max-w-[70%] mx-auto card my-10'>
-        <div className='flex justify-between'>
+      <section className='max-w-[70%] mx-auto card my-10 md:py-7 md:px-5'>
+        <div className='flex justify-between md:mb-5'>
           <h3 className='text-xl font-semibold'>Current Weather</h3>
           <label className="inline-flex items-center cursor-pointer">
             <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300 mr-3">C&deg;</span>
@@ -23,14 +23,14 @@ export const Home = ({cityInfo, weather}) => {
             <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">F&deg;</span>
           </label>
         </div>
-        <div className='flex items-center'>
+        <div className='flex items-center md:flex-col md:gap-5'>
           <section className='flex-1'>
             <h2 className='text-blue-900 font-bold'>{cityInfo?.name === 'La Merced' ? 'Bogotá' : cityInfo?.name}, {cityInfo?.sys?.country}</h2>
-            <div className='flex items-center'>
+            <div className='flex items-center md:flex-col'>
               <img className='w-36' src={Icons(cityInfo?.weather?.[0]?.main)} alt="Weather-icon" />
-              <span className='text-[100px] text-blue-900'>{toogle !== true ? Math.round(cityInfo?.main?.temp) : Math.round(9/5 * cityInfo?.main?.temp) + 32}&deg;</span>
+              <span className='md:p-0 text-[100px] text-blue-900'>{toogle !== true ? Math.round(cityInfo?.main?.temp) : Math.round(9/5 * cityInfo?.main?.temp) + 32}&deg;</span>
             </div>
-            <span className='text-2xl semibold text-gray-400'>{cityInfo?.weather?.[0]?.main}</span>
+            <span className='md:mb-5 block text-2xl semibold text-gray-400'>{cityInfo?.weather?.[0]?.main}</span>
           </section>
           <section className='flex flex-1 flex-col gap-5 text-xl text-gray-500'>
             <span className='text-2xl semibold text-gray-800'>Feels like {Math.round(cityInfo?.main?.feels_like)}&deg;C</span>
@@ -59,11 +59,11 @@ export const Home = ({cityInfo, weather}) => {
           </section>
         </div>
       </section>
-      <section className='max-w-[70%] mx-auto card mb-10'>
-        <h3 className='text-xl font-semibold mb-5'>Extended Forecast</h3>
-        <div className='flex justify-between'>
+      <section className='max-w-[70%] mx-auto card my-10 md:py-7 md:px-5'>
+        <h3 className='text-xl font-semibold mb-5 md:text-center'>Extended Forecast</h3>
+        <div className='flex justify-between md:flex-col md:gap-5'>
           {weather.filter((x,i) => i % 8 === 0).map((x,i) => (
-            <section key={i} className='flex flex-col items-center'>
+            <section key={i} className='flex flex-col items-center md:pb-5 border-solid border-gray-300 border-b-2'>
               <h5 className='text-2xl text-blue-900 font-bold text-center'>{DayOfTheWeek((day.getDay()+i)% 7)}</h5>
               <img className='w-24' src={Icons(x.weather[0].main)} alt="Weather-icon" />
               <span className='text-2xl font-semibold text-gray-600 text-center block mb-2'>{x.weather[0].main}</span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      md: {'max': '1000px'}
+    },
     extend: {},
   },
   plugins: [],
